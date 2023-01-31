a3fb39268bdbc614d7b5cbbf774a927865159fe1
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-31 16:05:33 -0600

    test: Add basic test scaffolding for Turn.js

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,9 @@
 class Turn {
-
+//constructor will take in a user guess and a Card object
+    constructor(userGuess, card) {
+    this.userGuess = userGuess;
+    this.card = card;
+    }
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,21 +5,40 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
-    var turn;
+    let card;
+    let turn;
 
     this.beforeEach(() => {
-        turn = new Turn();
-    })
-
-    it('should be a function', () => {
-        expect(Turn).to.be.a('function');
+        card = new Card(12, "Which iteration method returns an array of the same length as the original array?", ["map()", "forEach()", "reduce()"], "map()");
+        turn = new Turn("forEach()", card);
     })
 
     it('should be an instance of Turn', () => {
+        expect(Turn).to.be.a('function');
         expect(turn).to.be.an.instanceof(Turn);
     })
 
     it('should be able to take in a user guess', () => {
+        expect(turn.userGuess).to.equal("forEach()");
+    })
+
+    it('should take in an instantiation of Card for the card in play', () => {
+        expect(turn.card).to.equal(card);
+    })
+
+    it('should be able to return the user\'s guess', () => {
+
+    })
+
+    it('should be able to return the Card', () => {
+
+    })
+
+    it('should be able to evaluate the guess for correctness', () => {
+
+    })
+
+    it('should be able to give feedback to the user on their guess', () => {
         
     })
 });
